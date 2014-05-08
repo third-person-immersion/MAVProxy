e8f56cd19b48c341bb372a0b804845a3dd93325a
PopiT4 <tbsitu@gmail.com>
2014-05-08 19:40:22 +0200

    Add code for setting and holding altitude, Cleaned up code, Remove unneeded debug prints

--- MAVProxy/modules/mavproxy_quadcontrols.py
+++ MAVProxy/modules/mavproxy_quadcontrols.py
@@ -2,6 +2,7 @@
 '''quadcopter control commands'''
 
 import time, os, struct
+import threading
 from pymavlink import mavutil
 from MAVProxy.modules.lib import mp_module
 
@@ -14,7 +15,8 @@ class QuadcontrolsModule(mp_module.MPModule):
         self.add_command('yaw', self.cmd_yaw, "Yaw left or right", ['-100 - 100'])
         self.add_command('hover', self.cmd_hover, "Resets the vehicle to stand still in alt_hold mode")
         self.add_command('bend', self.cmd_bend, "Moves the vehicle in a curve movement", ['-100 - 100'])
-        self.add_command('initquadcontrol', self.cmd_initqc, "Initializes quadcontrol")
+        self.add_command('setalt', self.cmd_setAlt, "Sets the altitude", [0-100])
+        self.add_command('startalt', self.cmd_startAlt, 'Initializes the altitude hold thread')
 
 
     # Global variables used to control the vehicle, change these depending on your controller or APM
@@ -22,16 +24,43 @@ class QuadcontrolsModule(mp_module.MPModule):
     chanPitch=2
     chanThrottle=3
     chanYaw=4
-    #goalAltitude=-1 # Value -1 means it's not yet set
+    goalAltitude=-1 # Value -1 means it's not yet set
+    sweetSpot=0.3 # Sweet spot for altitude control
     minValue=800  # Min value of all sticks.
     midValue=1500 # Middle value of all sticks.
-    maxValue=2200 # Max value.
-    debugMode=True
+    maxValue=2200 # Max value of all sticks.
+    debugMode=True # If True several debug variables gets printed
 
-    def cmd_initqc(self, test):
-        self.cmd_movecopter([self.chanThrottle, self.midValue])
-        mavproxy.mpstate.functions.process_stdin("mode alt_hold")
-        print("Quadcontrol initialization done")
+
+    def cmd_startAlt(self, unused):
+        print("Altitude thread starting...")
+        t1 = threading.Thread(target=self.checkAlt)
+        t1.daemon = True
+        t1.start()
+        print("Altitude thread started")
+
+    # Check if the altitude is good, runs in a seperate thread    
+    def checkAlt(self):
+        while True:
+            time.sleep(0.5)
+            if self.goalAltitude != -1:
+                alt=float(self.mpstate.status.altitude)
+                if self.debugMode == True:
+                    print("alt: ", alt)
+                    print("goal ", self.goalAltitude)
+                if (abs(self.goalAltitude-alt)>self.sweetSpot):
+                    if (alt < self.goalAltitude):
+                        if self.debugMode == True:
+                            print("Altitude too low, adjusting...")
+                        self.cmd_movey([14]) # 14 correspond to around 1600
+                    elif (alt > self.goalAltitude):
+                        if self.debugMode == True:
+                            print("Altitude too high, adjusting...")
+                        self.cmd_movey([-29]) # -29 correspond to around 1300
+        
+    def cmd_setAlt(self, altitude):
+        self.goalAltitude=float(altitude[0])
+        print("New altitude set")
 
     def calcValue(self, procent):
         valproc=float(procent)/100
@@ -101,23 +130,21 @@ class QuadcontrolsModule(mp_module.MPModule):
         print("Done")
 
     
-    # Help function to movez, strafe and yaw
+    # Help function to movez, strafe, movey, hover and yaw
     def cmd_movecopter(self, args):
         if len(args) != 2:
             print("Error using cmd_movecopter, wrong number of arguments")
             return
         channel = int(args[0])
         valProcent = int(args[1])
-        #throttle=self.midValue
+
         if valProcent > 0:
             rawValue=self.midValue+(self.calcValue(abs(valProcent)))
         else:
             rawValue=self.midValue-(self.calcValue(abs(valProcent)))
         if self.debugMode:
             print("Throttling with value ", rawValue, " on channel ", channel)
-        #self.mpstate.functions.process_stdin("rc %d %d" % (self.chanThrottle, throttle))
         self.mpstate.functions.process_stdin("rc %d %d" % (channel, rawValue)) 
-        print("Done")
 
 
 def init(mpstate):
